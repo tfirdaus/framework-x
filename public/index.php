2eaf9568e95ca4b860e060fc2d6dad25ca865e73
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-06-11 23:53:52 +0700

    Add Twig

--- public/index.php
+++ public/index.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 
 use FrameworkX\App;
 use React\Http\Message\Response;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 $users = [['name' => 'Alice'], ['name' => 'Bob']];
+$loader = new FilesystemLoader(dirname(__DIR__) . '/resources/templates');
+$twig = new Environment($loader, ['cache' => dirname(__DIR__) . '/resources/templates/.cache']);
+$template = $twig->load('index.twig');
 
 $app = new App();
-$app->get('/', static fn () => Response::json($users));
+$app->get('/', static fn () => Response::html($template->render(['users' => $users])));
 $app->run();
