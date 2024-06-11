41b1b93440225cdf812277ccad708b729271a648
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-06-11 23:35:36 +0700

    Remove faker

--- public/index.php
+++ public/index.php
@@ -2,14 +2,11 @@
 
 declare(strict_types=1);
 
-use Faker\Factory;
 use FrameworkX\App;
 use React\Http\Message\Response;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-$faker = Factory::create();
-
 $app = new App();
-$app->get('/', static fn () => Response::plaintext($faker->sentence() . "\n"));
+$app->get('/', static fn () => Response::plaintext('Hello World!' . "\n"));
 $app->run();
