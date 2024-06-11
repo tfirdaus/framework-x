19d323a0960ce2b6c047a37e19e0e380e9c6fa94
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-06-11 23:39:26 +0700

    User JSON

--- public/index.php
+++ public/index.php
@@ -7,6 +7,8 @@ use React\Http\Message\Response;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
+$users = [['name' => 'Alice'], ['name' => 'Bob']];
+
 $app = new App();
-$app->get('/', static fn () => Response::plaintext('Hello World!' . "\n"));
+$app->get('/', static fn () => Response::json($users));
 $app->run();
