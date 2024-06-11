d75032ddce802d5f4b05591c23a2c8d5d2b34c34
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-06-11 23:24:30 +0700

    Add faker

--- public/index.php
+++ public/index.php
@@ -2,13 +2,16 @@
 
 declare(strict_types=1);
 
+use Faker\Factory;
 use FrameworkX\App;
 use React\Http\Message\Response;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
+$faker = Factory::create();
+
 $app = new App();
-$app->get('/', static function () {
-	return Response::plaintext('Hello world!');
+$app->get('/', static function () use ($faker) {
+	return Response::plaintext($faker->sentence());
 });
 $app->run();
